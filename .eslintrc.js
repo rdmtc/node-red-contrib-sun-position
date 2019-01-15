ef00fe0f0493132db1f43d363eed0c4189cb2a1d
robdg80 <robatwork@gesternet.de>
2019-01-15 17:33:33 +0100

    fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,79 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true,
-        "browser": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "space",
-            4,
-            {
-                "SwitchCase": 1
-            }
-        ],
-        "no-console": "off",
-        "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-        "no-useless-escape": "warn",
-        "no-constant-condition": "off",
-        "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 1 }],
-        "no-var": "error",
-        "prefer-const": "error",
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "single",
-            {
-                "avoidEscape": true,
-                "allowTemplateLiterals": true
-            }
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "comma-dangle": ["error", {
-            "arrays": "never",
-            "objects": "never",
-            "imports": "never",
-            "exports": "never",
-            "functions": "ignore"
-        }],
-        "no-trailing-spaces": "error"
-    }
-}
+  env: {
+    es6: true,
+    node: true,
+    browser: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {
+    indent: [
+      "error",
+      "space",
+      4,
+      {
+        SwitchCase: 1
+      }
+    ],
+    "max-len": [
+      "error",
+      {
+        code: 250,
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreRegExpLiterals: true,
+        ignoreTemplateLiterals: true
+      }
+    ],
+    "no-console": "off",
+    "no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_"
+      }
+    ],
+    "no-useless-escape": "warn",
+    "no-constant-condition": "off",
+    "no-multiple-empty-lines": [
+      "error",
+      {
+        max: 1,
+        maxEOF: 1
+      }
+    ],
+    "no-var": "error",
+    "prefer-const": "error",
+    "linebreak-style": ["error", "windows"],
+    "brace-style": [
+      2,
+      "1tbs",
+      {
+        allowSingleLine: true
+      }
+    ],
+    quotes: [
+      "error",
+      "single",
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ],
+    semi: ["error", "always"],
+    "comma-dangle": [
+      "error",
+      {
+        arrays: "never",
+        objects: "never",
+        imports: "never",
+        exports: "never",
+        functions: "ignore"
+      }
+    ],
+    "no-trailing-spaces": "error"
+  }
+};
--- nodes/time-calc.js
+++ nodes/time-calc.js
@@ -2,32 +2,52 @@
  * time-calc:
  *********************************************/
 "use strict";
-const util = require('util');
+const util = require("util");
 
-const path = require('path');
-const hlp = require(path.join(__dirname, '/lib/sunPosHelper.js'));
+const path = require("path");
+const hlp = require(path.join(__dirname, "/lib/sunPosHelper.js"));
 //const cron = require("cron");
 
 module.exports = function (RED) {
     "use strict";
 
-    function tsGetOperandData(node, msg, type, value, format, offset, multiplier) {
+    function tsGetOperandData(
+        node,
+        msg,
+        type,
+        value,
+        format,
+        offset,
+        multiplier
+    ) {
         let result = {};
         if (type == null || type === "none" || type === "") {
             return Date.now();
-        } else if (type === "entered" || type === "pdsTime" || type === "pdmTime" || type === "date") {
+        } else if (
+            type === "entered" ||
+            type === "pdsTime" ||
+            type === "pdmTime" ||
+            type === "date"
+        ) {
             result = node.positionConfig.getTimeProp(node, msg, type, value);
             if (result == null) {
-                throw new Error("could not evaluate " + type + '.' + value);
+                throw new Error("could not evaluate " + type + "." + value);
             } else if (result.error) {
-                throw new Error('error on getting operand: ' + result.error);
+                throw new Error("error on getting operand: " + result.error);
             }
-        } else { //msg, flow, global, str, num, env
+        } else {
+            //msg, flow, global, str, num, env
             let data = RED.util.evaluateNodeProperty(value, type, node, msg);
             if (!data) {
-                throw new Error("could not evaluate " + type + '.' + value);
+                throw new Error("could not evaluate " + type + "." + value);
             }
-            result.value = hlp.parseDateFromFormat(data, format, RED._("time-calc.days"), RED._("time-calc.month"), RED._("time-calc.dayDiffNames"));
+            result.value = hlp.parseDateFromFormat(
+                data,
+                format,
+                RED._("time-calc.days"),
+                RED._("time-calc.month"),
+                RED._("time-calc.dayDiffNames")
+            );
 
             if (result.value === "Invalid Date" || isNaN(result.value)) {
                 throw new Error("could not evaluate format of " + data);
@@ -45,7 +65,7 @@ module.exports = function (RED) {
 
     function tsGetPropData(node, msg, type, value, format, offset, days) {
         if (type == null || type === "none" || type === "") {
-            if (value === "" || (typeof value === 'undefined')) {
+            if (value === "" || typeof value === "undefined") {
                 return Date.now();
             } else {
                 return value;
@@ -54,10 +74,30 @@ module.exports = function (RED) {
             return node.positionConfig.getSunCalc(msg.ts);
         } else if (type === "pdmCalcData") {
             return node.positionConfig.getMoonCalc(msg.ts);
-        } else if (type === "entered" || type === "pdsTime" || type === "pdmTime" || type === "date") {
-            let data = node.positionConfig.getTimeProp(node, msg, type, value, offset, 1, days);
+        } else if (
+            type === "entered" ||
+            type === "pdsTime" ||
+            type === "pdmTime" ||
+            type === "date"
+        ) {
+            let data = node.positionConfig.getTimeProp(
+                node,
+                msg,
+                type,
+                value,
+                offset,
+                1,
+                days
+            );
             if (!data.error) {
-                return hlp.getFormatedDateOut(data.value, format, false, RED._("time-inject.days"), RED._("time-inject.month"), RED._("time-inject.dayDiffNames"));
+                return hlp.getFormatedDateOut(
+                    data.value,
+                    format,
+                    false,
+                    RED._("time-inject.days"),
+                    RED._("time-inject.month"),
+                    RED._("time-inject.dayDiffNames")
+                );
             }
             return data;
         }
@@ -72,120 +112,183 @@ module.exports = function (RED) {
         let operator = config.operator || 0;
         var node = this;
 
-        this.on('input', msg => {
+        this.on("input", msg => {
             try {
-                node.debug('input ' + util.inspect(msg));
-                if (node.positionConfig == null ||
+                node.debug("input " + util.inspect(msg));
+                if (
+                    node.positionConfig == null ||
                     config.operator == null ||
-                    config.operand1Type == null) {
-                    throw new Error('Configuration is missing!!');
+                    config.operand1Type == null
+                ) {
+                    throw new Error("Configuration is missing!!");
                 }
                 let operand1 = tsGetOperandData(this, msg, config.operand1Type, config.operand1, config.operand1Format, config.operand1Offset, config.operand1OffsetMultiplier);
-                node.debug('operand1 ' + util.inspect(operand1));
-                let operand2 = null;
-                let result = false;
-                let resObj = null;
 
-                if (operator > 0) {
-                    let useAltOper = true;
-                    if (config.operand2PropertyType !== 'none' &&
-                        config.operand2Type !== 'none') {
-                        let res = RED.util.evaluateNodeProperty(config.operand2Property, config.operand2PropertyType, node, msg);
-                        useAltOper = ((res == true) || (res == 'true'));
-                    }
-                    if (useAltOper) {
-                        operand2 = tsGetOperandData(this, msg, config.operand2AltType, config.operand2Alt, config.operand2AltFormat, config.operand2AltOffset, config.operand2AltOffsetMultiplier);
-                    } else {
-                        operand2 = tsGetOperandData(this, msg, config.operand2Type, config.operand2, config.operand2Format, config.operand2Offset, config.operand2OffsetMultiplier);
-                    }
-                    node.debug('operand2 ' + util.inspect(operand2));
-                    node.debug('operator ' + util.inspect(operator));
-                    switch (operator) {
-                        case 1: //greater
-                            result = operand1.getTime() > operand2.getTime();
-                            break;
-                        case 2: //greaterOrEqual
-                            result = operand1.getTime() >= operand2.getTime();
-                            break;
-                        case 3: //lesser
-                            result = operand1.getTime() < operand2.getTime();
-                            break;
-                        case 4: //lesserOrEqual
-                            result = operand1.getTime() <= operand2.getTime();
-                            break;
-                        case 5: //equal
-                            result = operand1.getTime() == operand2.getTime();
-                            break;
-                        case 6: //unequal
-                            result = operand1.getTime() != operand2.getTime();
-                            break;
-                        case 7: //equalSec
-                            operand1.setMilliseconds(0);
-                            operand2.setMilliseconds(0);
-                            result = operand1.getTime() == operand2.getTime();
-                            break;
-                        case 8: //unequalSec
-                            operand1.setMilliseconds(0);
-                            operand2.setMilliseconds(0);
-                            result = operand1.getTime() != operand2.getTime();
-                            break;
-                        case 9: //equalMin
-                            operand1.setMilliseconds(0);
-                            operand1.setSeconds(0);
-                            operand2.setMilliseconds(0);
-                            operand2.setSeconds(0);
-                            result = operand1.getTime() == operand2.getTime();
-                            break;
-                        case 10: //unequalMin
-                            operand1.setMilliseconds(0);
-                            operand1.setSeconds(0);
-                            operand2.setMilliseconds(0);
-                            operand2.setSeconds(0);
-                            result = operand1.getTime() != operand2.getTime();
-                            break;
-                    }
-                }
-
-                if (config.result1Type !== 'none' && config.result1Value) {
+                if (config.result1Type !== "none" && config.result1Value) {
                     let resObj = null;
-                    if (config.result1Type == 'operand1') {
+                    if (config.result1Type == "operand1") {
                         resObj = hlp.getFormatedDateOut(operand1, config.result1Format, false, RED._("time-inject.days"), RED._("time-inject.month"), RED._("time-inject.dayDiffNames"));
-                    } else if (config.result1Type == 'operand2') {
-                        resObj = hlp.getFormatedDateOut(operand2, config.result1Format, false, RED._("time-inject.days"), RED._("time-inject.month"), RED._("time-inject.dayDiffNames"));
                     } else {
                         resObj = tsGetPropData(node, msg, config.result1ValueType, config.result1Value, config.result1Format, config.result1Offset);
                     }
-                    node.debug('resObj ' + util.inspect(resObj));
+                    node.debug("resObj " + util.inspect(resObj));
                     if (resObj == null) {
-                        throw new Error("could not evaluate " + config.result1ValueType + '.' + config.result1Value);
+                        throw new Error("could not evaluate " + config.result1ValueType + "." + config.result1Value);
                     } else if (resObj.error) {
-                        this.error('error on getting result: ' + resObj.error);
-                    } else if (config.result1Type === 'msg' || config.result1Type === 'msgProperty') {
+                        this.error("error on getting result: " + resObj.error);
+                    } else if (config.result1Type === "msg" || config.result1Type === "msgProperty") {
                         RED.util.setMessageProperty(msg, name, resObj);
-                    } else if ((config.result1Type === 'flow' || config.result1Type === 'global') && ((operator <= 0) || result)) {
+                    } else if ((config.result1Type === "flow" || config.result1Type === "global") && (operator <= 0 || result)) {
                         let contextKey = RED.util.parseContextStore(name);
                         node.context()[type].set(contextKey.key, resObj, contextKey.store);
                     }
                 }
 
-                if ((operator > 0) && result) {
-                    node.send([msg, null]);
-                } else if (operator > 0) {
-                    node.send([null, msg]);
+                node.debug("operand1 " + util.inspect(operand1));
+                let resObj = null;
+                let rules = config.rules;
+                let rulesLength = rules.length;
+                for (let i = 0; i < rulesLength; ++i) {
+                    let rule = rules[i];
+                    let operatorValid = true;
+                    if (rule.propertyType !== "none") {
+                        let res = RED.util.evaluateNodeProperty(rule.propertyValue, rule.propertyType, node, msg);
+                        operatorValid = res == true || res == "true";
+                    }
+                    if (operatorValid) {
+                        let ruleoperand = tsGetOperandData(this, msg, rule.operandType, rule.operandValue, rule.format, rule.offsetType, rule.offsetValue, rule.multiplier);
+                        node.debug("operand " + util.inspect(ruleoperand));
+                        node.debug("operator " + util.inspect(rule.operator));
+                        let result = false;
+                        let inputOperant = new Date(operand1);
+
+                        let opHigh = Math.trunc(rule.operator / 10);
+                        let opLow = rule.operator % 10;
+
+                        switch (opHigh) {
+                            case 2: //sec
+                                inputOperant.setMilliseconds(0);
+                                ruleoperand.setMilliseconds(0);
+                                break;
+                            case 2: //min
+                                inputOperant.setMilliseconds(0);
+                                inputOperant.setSeconds(0);
+                                ruleoperand.setMilliseconds(0);
+                                ruleoperand.setSeconds(0);
+                                break;
+                            case 3: //hour
+                                inputOperant.setMilliseconds(0);
+                                inputOperant.setSeconds(0);
+                                inputOperant.setMinutes(0);
+                                ruleoperand.setMilliseconds(0);
+                                ruleoperand.setSeconds(0);
+                                ruleoperand.setMinutes(0);
+                                break;
+                            case 4: //date
+                                inputOperant.setMilliseconds(0);
+                                inputOperant.setSeconds(0);
+                                inputOperant.setMinutes(0);
+                                inputOperant.setHours(0);
+                                ruleoperand.setMilliseconds(0);
+                                ruleoperand.setSeconds(0);
+                                ruleoperand.setMinutes(0);
+                                ruleoperand.setHours(0);
+                                break;
+                            case 5: //month
+                                inputOperant.setMilliseconds(0);
+                                inputOperant.setSeconds(0);
+                                inputOperant.setMinutes(0);
+                                inputOperant.setHours(0);
+                                inputOperant.setDate(0);
+                                ruleoperand.setMilliseconds(0);
+                                ruleoperand.setSeconds(0);
+                                ruleoperand.setMinutes(0);
+                                ruleoperand.setHours(0);
+                                ruleoperand.setDate(0);
+                                break;
+                            case 11: //only sec
+                                inputOperant = inputOperant.getSeconds();
+                                ruleoperand = ruleoperand.getSeconds();
+                                break;
+                            case 12: //only min
+                                inputOperant = inputOperant.getMinutes();
+                                ruleoperand = ruleoperand.getMinutes();
+                                break;
+                            case 13: //only hour
+                                inputOperant = inputOperant.getHours();
+                                ruleoperand = ruleoperand.getHours();
+                                break;
+                            case 14: //only day
+                                inputOperant = inputOperant.getDate();
+                                ruleoperand = ruleoperand.getDate();
+                                break;
+                            case 15: //only dayOfWeek
+                                inputOperant = inputOperant.getDay() + 1;
+                                ruleoperand = ruleoperand.getDay() + 1;
+                                break;
+                            case 16: //only Month
+                                inputOperant = inputOperant.getMonth() + 1;
+                                ruleoperand = ruleoperand.getMonth() + 1;
+                                break;
+                            case 17: //only FullYear
+                                inputOperant = inputOperant.getFullYear();
+                                ruleoperand = ruleoperand.getFullYear();
+                                break;
+                        }
+                        switch (opLow) {
+                            case 1: //equal             { id: 1, group: "ms", label: "==", "text": "equal" },
+                                result = inputOperant.getTime() == ruleoperand.getTime();
+                                break;
+                            case 2: //unequal           { id: 2, group: "ms", label: "!=", "text": "unequal" },
+                                result = inputOperant.getTime() != ruleoperand.getTime();
+                                break;
+                            case 3: //greater           { id: 3, group: "ms", label: ">", "text": "greater" },
+                                result = inputOperant.getTime() > ruleoperand.getTime();
+                                break;
+                            case 4: //greaterOrEqual    { id: 5, group: "ms", label: ">=", "text": "greater or equal" },
+                                result = inputOperant.getTime() >= ruleoperand.getTime();
+                                break;
+                            case 5: //lesser            { id: 6, group: "ms", label: "<", "text": "lesser" },
+                                result = inputOperant.getTime() < ruleoperand.getTime();
+                                break;
+                            case 6: //lesserOrEqual     { id: 7, group: "ms", label: "<=", "text": "lesser or equal" },
+                                result = inputOperant.getTime() <= ruleoperand.getTime();
+                                break;
+                        }
+
+                        if (result) {
+                            resObj.push(msg);
+                        } else {
+                            resObj.push(null);
+                        }
+                        if (!config.checkall) {
+                            break;
+                        }
+                    }
                 }
-                node.send(msg);
+                for (let i = resObj.length; i < rulesLength; ++i) {
+                    resObj.push(null);
+                }
+                resObj.push(msg);
+                return resObj;
             } catch (err) {
-                hlp.errorHandler(this, err, RED._("time-calc.errors.error-text"), RED._("time-calc.errors.error-title"));
+                hlp.errorHandler(
+                    this,
+                    err,
+                    RED._("time-calc.errors.error-text"),
+                    RED._("time-calc.errors.error-title")
+                );
             }
         });
     }
-    RED.nodes.registerType('time-calc', timeCalcNode);
+    RED.nodes.registerType("time-calc", timeCalcNode);
 
-    RED.httpAdmin.get('/sun-position/js/*', function(req,res) {
-        var options = {
-          root: __dirname + '/static/',
-          dotfiles: 'deny'
-        };
-        res.sendFile(req.params[0], options);
-    });
+    /*
+      RED.httpAdmin.get('/sun-position/js/*', function(req,res) {
+          var options = {
+            root: __dirname + '/static/',
+            dotfiles: 'deny'
+          };
+          res.sendFile(req.params[0], options);
+      });/* */
 };
