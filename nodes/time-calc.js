4fa9b3196365e229f82a54b3033245542691bbbb
robdg80 <robatwork@gesternet.de>
2019-01-16 16:22:30 +0100

    development

--- nodes/time-calc.js
+++ nodes/time-calc.js
@@ -159,110 +159,105 @@ module.exports = function (RED) {
                         let ruleoperand = tsGetOperandData(this, msg, rule.operandType, rule.operandValue, rule.format, rule.offsetType, rule.offsetValue, rule.multiplier);
                         node.debug("operand " + util.inspect(ruleoperand));
                         node.debug("operator " + util.inspect(rule.operator));
-                        let result = false;
-                        let inputOperant = new Date(operand1);
+                        node.debug("operatorType " + util.inspect(rule.operatorType));
 
-                        let opHigh = Math.trunc(rule.operator / 10);
-                        let opLow = rule.operator % 10;
-
-                        switch (opHigh) {
-                            case 2: //sec
-                                inputOperant.setMilliseconds(0);
-                                ruleoperand.setMilliseconds(0);
-                                break;
-                            case 2: //min
-                                inputOperant.setMilliseconds(0);
-                                inputOperant.setSeconds(0);
-                                ruleoperand.setMilliseconds(0);
-                                ruleoperand.setSeconds(0);
-                                break;
-                            case 3: //hour
-                                inputOperant.setMilliseconds(0);
-                                inputOperant.setSeconds(0);
-                                inputOperant.setMinutes(0);
-                                ruleoperand.setMilliseconds(0);
-                                ruleoperand.setSeconds(0);
-                                ruleoperand.setMinutes(0);
-                                break;
-                            case 4: //date
-                                inputOperant.setMilliseconds(0);
-                                inputOperant.setSeconds(0);
-                                inputOperant.setMinutes(0);
-                                inputOperant.setHours(0);
-                                ruleoperand.setMilliseconds(0);
-                                ruleoperand.setSeconds(0);
-                                ruleoperand.setMinutes(0);
-                                ruleoperand.setHours(0);
-                                break;
-                            case 5: //month
-                                inputOperant.setMilliseconds(0);
-                                inputOperant.setSeconds(0);
-                                inputOperant.setMinutes(0);
-                                inputOperant.setHours(0);
-                                inputOperant.setDate(0);
-                                ruleoperand.setMilliseconds(0);
-                                ruleoperand.setSeconds(0);
-                                ruleoperand.setMinutes(0);
-                                ruleoperand.setHours(0);
-                                ruleoperand.setDate(0);
-                                break;
-                            case 11: //only sec
-                                inputOperant = inputOperant.getSeconds();
-                                ruleoperand = ruleoperand.getSeconds();
-                                break;
-                            case 12: //only min
-                                inputOperant = inputOperant.getMinutes();
-                                ruleoperand = ruleoperand.getMinutes();
-                                break;
-                            case 13: //only hour
-                                inputOperant = inputOperant.getHours();
-                                ruleoperand = ruleoperand.getHours();
-                                break;
-                            case 14: //only day
-                                inputOperant = inputOperant.getDate();
-                                ruleoperand = ruleoperand.getDate();
-                                break;
-                            case 15: //only dayOfWeek
-                                inputOperant = inputOperant.getDay() + 1;
-                                ruleoperand = ruleoperand.getDay() + 1;
-                                break;
-                            case 16: //only Month
-                                inputOperant = inputOperant.getMonth() + 1;
-                                ruleoperand = ruleoperand.getMonth() + 1;
-                                break;
-                            case 17: //only FullYear
-                                inputOperant = inputOperant.getFullYear();
-                                ruleoperand = ruleoperand.getFullYear();
-                                break;
-                        }
-                        switch (opLow) {
+                        let compare = null;
+                        switch (rule.operator) {
                             case 1: //equal             { id: 1, group: "ms", label: "==", "text": "equal" },
-                                result = inputOperant.getTime() == ruleoperand.getTime();
+                                compare = (op1, op2) => op1 == op2;
                                 break;
                             case 2: //unequal           { id: 2, group: "ms", label: "!=", "text": "unequal" },
-                                result = inputOperant.getTime() != ruleoperand.getTime();
+                                compare = (op1, op2) => op1 != op2;
                                 break;
                             case 3: //greater           { id: 3, group: "ms", label: ">", "text": "greater" },
-                                result = inputOperant.getTime() > ruleoperand.getTime();
+                                compare = (op1, op2) => op1 > op2;
                                 break;
                             case 4: //greaterOrEqual    { id: 5, group: "ms", label: ">=", "text": "greater or equal" },
+                                compare = (op1, op2) => op1 >= op2;
                                 result = inputOperant.getTime() >= ruleoperand.getTime();
                                 break;
                             case 5: //lesser            { id: 6, group: "ms", label: "<", "text": "lesser" },
-                                result = inputOperant.getTime() < ruleoperand.getTime();
+                                compare = (op1, op2) => op1 < op2;
                                 break;
                             case 6: //lesserOrEqual     { id: 7, group: "ms", label: "<=", "text": "lesser or equal" },
-                                result = inputOperant.getTime() <= ruleoperand.getTime();
+                                compare = (op1, op2) => op1 <= op2;
                                 break;
+                        }
+                        let result = false;
+                        if (compare) {
+                            let inputOperant = new Date(operand1);
+                            //result = inputOperant.getTime() <= ruleoperand.getTime();
+                            if (rule.operatorType !== '*' && typeof rule.operatorType !== "undefined") {
+                                switch (rule.operatorType) {
+                                    case '11': //ms
+                                        result = compare(inputOperant.getMilliseconds(), ruleoperand.getMilliseconds());
+                                        break;
+                                    case '12': //only sec
+                                        result = compare(inputOperant.getSeconds(), ruleoperand.getSeconds());
+                                        break;
+                                    case '13': //only min
+                                        result = compare(inputOperant.getMinutes(), ruleoperand.getMinutes());
+                                        break;
+                                    case '14': //only hour
+                                        result = compare(inputOperant.getHours(), ruleoperand.getHours());
+                                        break;
+                                    case '15': //only day
+                                        result = compare(inputOperant.getDate(), ruleoperand.getDate());
+                                        break;
+                                    case '16': //only Month
+                                        result = compare(inputOperant.getMonth(), ruleoperand.getMonth());
+                                        break;
+                                    case '17': //only FullYear
+                                        result = compare(inputOperant.getFullYear(), ruleoperand.getFullYear());
+                                        break;
+                                    case '18': //only dayOfWeek
+                                        result = compare(inputOperant.getDay(), ruleoperand.getDay());
+                                        break;
+                                    default:
+                                        if (rule.operatorType.indexOf('11') < 0) {
+                                            inputOperant.setMilliseconds(0);
+                                            ruleoperand.setMilliseconds(0);
+                                        }
+                                        if (rule.operatorType.indexOf('12') < 0) {
+                                            inputOperant.setSeconds(0);
+                                            ruleoperand.setSeconds(0);
+                                        }
+                                        if (rule.operatorType.indexOf('13') < 0) {
+                                            inputOperant.setMinutes(0);
+                                            ruleoperand.setMinutes(0);
+                                        }
+                                        if (rule.operatorType.indexOf('14') < 0) {
+                                            inputOperant.setHours(0);
+                                            ruleoperand.setHours(0);
+                                        }
+                                        if (rule.operatorType.indexOf('15') < 0) {
+                                            inputOperant.setDate(0);
+                                            ruleoperand.setDate(0);
+                                        }
+                                        if (rule.operatorType.indexOf('16') < 0) {
+                                            inputOperant.setMonth(0);
+                                            ruleoperand.setMonth(0);
+                                        }
+                                        if (rule.operatorType.indexOf('17') < 0) {
+                                            inputOperant.setFullYear(0);
+                                            ruleoperand.setFullYear(0);
+                                        }
+                                        result = compare(inputOperant.getTime(), ruleoperand.getTime());
+                                        if (rule.operatorType.indexOf('18') >= 0) {
+                                            result = result && compare(inputOperant.getDay(), ruleoperand.getDay());
+                                        }
+                                        break;
+                                }
+                            }
                         }
 
                         if (result) {
                             resObj.push(msg);
+                            if (!config.checkall) {
+                                break;
+                            }
                         } else {
                             resObj.push(null);
-                        }
-                        if (!config.checkall) {
-                            break;
                         }
                     }
                 }
