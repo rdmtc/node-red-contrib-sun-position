b4ed4849eaa4211afc3f37aa93becd670dc3c7ce
Hypnos <robatwork@gesternet.de>
2019-03-05 11:19:58 +0100

    added compatibility for  node-red-contrib-blindcontroller

--- nodes/sun-position.js
+++ nodes/sun-position.js
@@ -16,10 +16,27 @@ module.exports = function (RED) {
         this.topic = config.topic || '';
         this.rules = config.rules || [];
         this.azimuthPos = {};
+        this.start = config.start;
+        this.startType = config.startType || 'none';
+        this.startOffset = config.startOffset || 0;
+        this.startOffsetType = config.startOffsetType || 'none';
+        this.startOffsetMultiplier = config.startOffsetMultiplier || 60;
+        this.end = config.end;
+        this.endType = config.endType || 'none';
+        this.endOffset = config.endOffset || 0;
+        this.endOffsetType = config.endOffsetType || 'none';
+        this.endOffsetMultiplier = config.endOffsetMultiplier || 60;
         const node = this;
 
         this.on('input', function (msg) {
             try {
+                let now = new Date();
+                if (typeof msg.time !== 'undefined') {
+                    now = new Date(msg.time);
+                }
+                if (typeof msg.ts !== 'undefined') {
+                    now = new Date(msg.time);
+                }
                 const ports = new Array(this.rules.length);
                 ports[0] = {
                     payload: this.positionConfig.getSunCalc(msg.ts),
@@ -33,6 +50,21 @@ module.exports = function (RED) {
 
                 ports[0].payload.pos = [];
                 ports[0].payload.posChanged = false;
+
+                if (node.startType !== 'none') {
+                    ports[0].startTime = node.positionConfig.getTimeProp(node, msg, node.startType, node.start, node.startOffset, node.startOffsetType, node.startOffsetMultiplier).getTime();
+                }
+
+                if (node.endType !== 'none') {
+                    ports[0].endTime = node.positionConfig.getTimeProp(node, msg, node.endType, node.end, node.endOffset, node.endOffsetType, node.endOffsetMultiplier).getTime();
+                }
+                let statusSunInSky = false;
+                if (ports[0].startTime && ports[0].endTime) {
+                    const nowMillis = now.getTime();
+                    ports[0].sunInSky = nowMillis > ports[0].startTime && nowMillis < ports[0].endTime;
+                    statusSunInSky = true;
+                }
+
                 for (let i = 0; i < this.rules.length; i += 1) {
                     const rule = this.rules[i];
                     const low = getNumProp(node, msg, rule.valueLowType, rule.valueLow);
@@ -44,16 +76,36 @@ module.exports = function (RED) {
                     if (chk) {
                         ports[i + 1] = RED.util.cloneMessage(msg);
                         ports[i + 1].posChanged = chg;
+                        ports[i + 1].sunInSky = chk;
                     }
                 }
 
                 node.azimuthPos = ports[0].payload.pos;
                 this.send(ports);
-                this.status({
-                    fill: 'grey',
-                    shape: 'dot',
-                    text: ports[0].payload.azimuth.toFixed(2) + '/' + ports[0].payload.altitude.toFixed(2) + ' - ' + ports[0].payload.lastUpdate.toLocaleString()
-                });
+
+                if (statusSunInSky) {
+                    if (ports[0].sunInSky === true) {
+                        node.status({
+                            fill:   'yellow',
+                            shape:  'dot',
+                            text:   new Date(ports[0].startTime).toLocaleTimeString() + ' - ' +
+                                    new Date(ports[0].endTime).toLocaleTimeString()
+                        });
+                    } else {
+                        node.status({
+                            fill:   'blue',
+                            shape:  'dot',
+                            text:   new Date(ports[0].startTime).toLocaleTimeString() + ' - ' +
+                                    new Date(ports[0].endTime).toLocaleTimeString()
+                        });
+                    }
+                } else {
+                    this.status({
+                        fill:   'grey',
+                        shape:  'dot',
+                        text:   ports[0].payload.azimuth.toFixed(2) + '/' + ports[0].payload.altitude.toFixed(2) + ' - ' + ports[0].payload.lastUpdate.toLocaleString()
+                    });
+                }
                 return null;
             } catch (err) {
                 node.error(err.message);
